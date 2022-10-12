652d3c0172dadebc16e3c98a529342b053b7dd6a
Igor Kamyshev <igor@kamyshev.me>
2022-10-12 13:08:30 +0700

    Minimal styling

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -1,16 +1,26 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { ImageResponse } from "@vercel/og";
 
+const getStapelRegular = fetch(
+  new URL("../../assets/stapel_regular.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
 export const config = {
-  runtime: 'experimental-edge',
+  runtime: "experimental-edge",
 };
 
-export default function () {
+export default async function (req) {
+  const stapelRegular = await getStapelRegular;
+  const { searchParams } = req.nextUrl;
+
+  const expectedPrice = searchParams.get("expectedPrice");
+  const iata = searchParams.get("iata");
+
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 128,
-          background: "white",
+          background: "#0c73fe",
           width: "100%",
           height: "100%",
           display: "flex",
@@ -19,12 +29,29 @@ export default function () {
           justifyContent: "center",
         }}
       >
-        Hello world!
+        <div tw="flex flex-col">
+          <p tw="text-6xl text-white" style={{ fontFamily: "Stapel" }}>
+            Aviasales
+          </p>
+          <div tw="flex">
+            <p tw="text-2xl text-white">
+              {iata} от {expectedPrice} рублей
+            </p>
+          </div>
+        </div>
       </div>
     ),
     {
       width: 1200,
       height: 600,
+      fonts: [
+        {
+          name: "Stapel",
+          data: stapelRegular,
+          weight: 500,
+          style: "normal",
+        },
+      ],
     }
   );
 }
